c22500aa8601a24fb474566189f07883416f9391
Vladimir Zarypov <krre31@gmail.com>
2026-08-03 16:48:33 +0300

    Add optimize build option

--- build.zig
+++ build.zig
@@ -2,9 +2,11 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
     const mod = b.addModule("angie3d", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
     const mod_tests = b.addTest(.{
