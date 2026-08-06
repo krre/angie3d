dcd6d80af1b091fbb85be529fd8c1dc6e10432ce
Vladimir Zarypov <krre31@gmail.com>
2026-08-06 06:30:50 +0300

    Add `geometry` module

--- src/ui/ui.zig
+++ src/ui/ui.zig
@@ -1,1 +1,7 @@
+const geometry = @import("geometry.zig");
+pub const Pos2D = geometry.Pos2D;
+pub const Size2D = geometry.Size2D;
+pub const Pos3D = geometry.Pos3D;
+pub const Size3D = geometry.Size3D;
+
 pub const Color = @import("Color.zig");
