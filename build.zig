c756b3d2c01e6c46fd86e989501bdfff066c56f8
Vladimir Zarypov <krre31@gmail.com>
2026-08-08 12:37:42 +0300

    Add `hello-world` example

--- build.zig
+++ build.zig
@@ -1,10 +1,12 @@
 const std = @import("std");
 
+const angie3d = "angie3d";
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("angie3d", .{
+    const mod = b.addModule(angie3d, .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
@@ -31,4 +33,74 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
+
+    const examples_step = b.step(
+        "examples",
+        "Build all examples",
+    );
+
+    addExample(
+        b,
+        "hello-world",
+        optimize,
+        mod,
+        examples_step,
+    );
+}
+
+fn addExample(
+    b: *std.Build,
+    comptime name: []const u8,
+    optimize: std.builtin.OptimizeMode,
+    angie3d_mod: *std.Build.Module,
+    examples_step: *std.Build.Step,
+) void {
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .wasm32,
+        .os_tag = .freestanding,
+    });
+
+    const example = b.addExecutable(.{
+        .name = name,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path(
+                b.fmt("examples/{s}/src/root.zig", .{name}),
+            ),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    example.entry = .disabled;
+    example.root_module.addImport(angie3d, angie3d_mod);
+    example.root_module.export_symbol_names = angie3d_mod.export_symbol_names;
+
+    const install_wasm = b.addInstallArtifact(example, .{
+        .dest_dir = .{
+            .override = .{
+                .custom = b.fmt("examples/{s}/web", .{name}),
+            },
+        },
+        .dest_sub_path = "lib.wasm",
+    });
+
+    const install_angie3d_web = b.addInstallDirectory(.{
+        .source_dir = b.path("web"),
+        .install_dir = .prefix,
+        .install_subdir = b.fmt(
+            "examples/{s}/web",
+            .{name},
+        ),
+    });
+
+    install_angie3d_web.step.dependOn(&install_wasm.step);
+
+    const example_step = b.step(
+        b.fmt("{s}", .{name}),
+        b.fmt("Build {s} example", .{name}),
+    );
+
+    example_step.dependOn(&install_angie3d_web.step);
+
+    examples_step.dependOn(example_step);
 }
---
+++ examples/hello-world/src/root.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const angie3d = @import("angie3d");
+const console = angie3d.console;
+const js = angie3d.js;
+const Box = angie3d.ui.widget.Box;
+const Application = angie3d.ui.Application;
+
+const HelloWorld = struct {
+    pub var app: Application = undefined;
+    var root: Box = undefined;
+
+    pub fn init(allocator: Allocator) void {
+        root = Box.init(allocator);
+        app = Application.init(allocator, &root.widget);
+        Application.setTitle("Hello World!");
+    }
+};
+
+export fn start() void {
+    HelloWorld.init(std.heap.wasm_allocator);
+    js.event_handler = HelloWorld.app.eventHandler();
+    HelloWorld.app.render();
+}
