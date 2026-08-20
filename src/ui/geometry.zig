52fc6f1f0a1aa175429156259d463d4959981d31
Vladimir Zarypov <krre31@gmail.com>
2026-08-20 09:06:29 +0300

    Add zero constant to geometry types

--- src/ui/geometry.zig
+++ src/ui/geometry.zig
@@ -1,21 +1,29 @@
 pub const Pos2D = struct {
     x: i32,
     y: i32,
+
+    pub const zero: Pos2D = .{ .x = 0, .y = 0 };
 };
 
 pub const Size2D = struct {
     width: u32,
     height: u32,
+
+    pub const zero: Size2D = .{ .width = 0, .height = 0 };
 };
 
 pub const Pos3D = struct {
     x: f32,
     y: f32,
     z: f32,
+
+    pub const zero: Pos3D = .{ .x = 0, .y = 0, .z = 0 };
 };
 
 pub const Size3D = struct {
     width: f32,
     height: f32,
     depth: f32,
+
+    pub const zero: Size3D = .{ .width = 0, .height = 0, .depth = 0 };
 };
