f80cd51208e799416e4bbe8871482d4174412e3c
Vladimir Zarypov <krre31@gmail.com>
2026-08-26 08:46:22 +0300

    Fix placement functions of `Spatial` type

--- src/ui/spatial/spatial.zig
+++ src/ui/spatial/spatial.zig
@@ -6,18 +6,18 @@ const Pos3D = geometry.Pos3D;
 pub const Spatial = struct {
     node: Node,
     transform: Transform = .{},
+
+    pub fn init() Spatial {
+        return Spatial{
+            .node = Node.init(),
+        };
+    }
+
+    pub fn fromNode(node: *Node) *Spatial {
+        return @fieldParentPtr("node", node);
+    }
+
+    pub fn move(self: *Spatial, pos: Pos3D) void {
+        self.pos = pos;
+    }
 };
-
-pub fn init() Spatial {
-    return Spatial{
-        .node = Node.init(),
-    };
-}
-
-pub fn fromNode(node: *Node) *Spatial {
-    return @fieldParentPtr("node", node);
-}
-
-pub fn move(self: *Spatial, pos: Pos3D) void {
-    self.pos = pos;
-}
