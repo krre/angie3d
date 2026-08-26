5980cbdbc59233ee35ee0bf140fe16aadffe5439
Vladimir Zarypov <krre31@gmail.com>
2026-08-26 08:47:17 +0300

    Replace `Node` type by `Rectangle` for scene in `HelloWorldExample`

--- examples/hello-world/src/root.zig
+++ examples/hello-world/src/root.zig
@@ -1,19 +1,19 @@
 const angie3d = @import("angie3d");
-const Widget = angie3d.ui.widget.Widget;
 const Rectangle = angie3d.ui.widget.Rectangle;
 const Application = angie3d.core.Application;
 const View = angie3d.ui.View;
 const Node = angie3d.ui.node.Node;
 
 const HelloWorldExample = struct {
-    scene: Node,
+    scene: Rectangle,
 
     pub fn init(app: *Application) !HelloWorldExample {
         app.setTitle("Hello World Example");
-        var helloWorldExample = HelloWorldExample{ .scene = Node.init() };
+
+        var helloWorldExample = HelloWorldExample{ .scene = Rectangle.init() };
 
         app.setView(.{
-            .view = View.init(&helloWorldExample.scene),
+            .view = View.init(&helloWorldExample.scene.widget.spatial.node),
         });
 
         return helloWorldExample;
